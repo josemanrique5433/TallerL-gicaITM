d72f1873a2b4491c8e080c256b06f5467e4c6941
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-03-16 16:21:56 -0500

    Número par e Impar

--- numeroPar/Program.cs
+++ numeroPar/Program.cs
@@ -2,19 +2,43 @@
 {
     private static void Main(string[] args)
     {
-        Console.Write("Digite un número: ");
-        int numero = int.Parse(Console.ReadLine());
-
-        if (numero%2 == 0) 
+        String numberString = String.Empty;
+        do
         {
+            Console.Write("Digite un número entero ó la palabra 'Salir' para salir: ");
+            numberString = (Console.ReadLine());
+            if (numberString!.ToLower() == "salir")
+            {
+                continue;
+            }
 
-            Console.WriteLine($" El número {numero} es par");
-        }else
-        { 
-            Console.WriteLine($"El número {numero} es impar");
-        }
 
-        
+            var numenerInt = 0;
+            if (int.TryParse(numberString, out numenerInt))
+            {
+                if (numenerInt % 2 == 0)
+                {
+
+                    Console.WriteLine($" El número {numenerInt} es par");
+                }
+                else
+                {
+                    Console.WriteLine($"El número {numenerInt} es impar");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine($"Lo que ingresaste: {numberString} no es un número entero");
+            }
+
+
+
+
+
+        } while (numberString!.ToLower() != "salir");
+
+        Console.WriteLine("Game Over");
 
 
 
