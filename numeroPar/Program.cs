356c6d08207a77bd723cd56920dd8dc3c9a6f000
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-03-16 13:12:20 -0500

    Número par o impar

---
+++ numeroPar/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero%2 == 0) 
+        {
+
+            Console.WriteLine($" El número {numero} es par");
+        }else
+        { 
+            Console.WriteLine($"El número {numero} es impar");
+        }
+
+        
+
+
+
+    }
+}
