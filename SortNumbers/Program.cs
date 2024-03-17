15c01ee9b6e7bb0d18410365f401f2149dc3fc49
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-03-16 19:58:42 -0500

    sortnumbers

--- SortNumbers/Program.cs
+++ SortNumbers/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Shared;
+
+do
+{
+    Console.WriteLine("Ingrese tres números diferentes...");
+    var a = ConsolExtension.GetIn("Ingrese el primer número : ");
+    var b = ConsolExtension.GetIn("Ingrese el segundo número: ");
+    
+
+    if (a == b)
+    {
+        Console.WriteLine("Los números deben ser diferentes; vueva a empezar");
+        continue;
+    }
+    var c = ConsolExtension.GetIn("Ingrese el tercer número : ");
+    if (b == c || c==a)
+    {
+        Console.WriteLine("Los números deben ser diferentes; vueva a empezar");
+        continue;
+    }
+    if (a > b && a > c)
+    {
+        if (b > c)
+        {
+            Console.WriteLine($" EL número mayor es {a}, el de le medio es {b}, el menor es {c}");
+        }
+        else
+        {
+            Console.WriteLine($" EL número mayor es {a}, el de le medio es {c}, el menor es {b}");
+        }
+
+    }
+    else if (b > a && b > c)
+    {
+        if (a > c) 
+        {
+       Console.WriteLine($" EL número mayor es {b}, el de el medio es {a}, el menor es {c}");
+        }
+        else
+        {
+            Console.WriteLine($" EL número mayor es {b}, el de el medio es {c}, el menor es {a}");
+        }
+    }
+    else
+    {
+        if (a > b && b>c)
+        {
+            Console.WriteLine($" EL número mayor es {c}, el de el medio es {a}, el menor es {b}");
+        }
+        else
+        {
+            Console.WriteLine($" EL número mayor es {c}, el de el medio es {b}, el menor es {a}");
+        }
+    }
+
+} while (true);
